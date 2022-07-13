b648dc61a05fc05b0b3f927b680153412bec979c
nanojin <nanochemic@gmail.com>
2022-07-12 21:03:24 -1000

    cleanup + idea
    added details to comment

--- r34_img_size.js
+++ r34_img_size.js
@@ -40,13 +40,11 @@
 		width: `auto`
 	})
 
-	// Something random. L += dL = (1 - (300 / (300 + n_0)))
+	// Something random. L += dL = (1 - (300 / (300 + n_0))) // This is a leveling idea for gained XP after a game. It's based on 100 points, with an average of 200 points per session. The curve flattens as x approaches infinity. The maximum XP gained is always < 1.
 
 	const body = document.body;
-	// const frame = {x: body.clientWidth, y: body.clientHeight};
 	const rc = document.querySelector('#right-col')
 	const parent = rc.parentElement
-	// const frame = {x: parent.offsetLeft, y: parent.offsetTop};
 	
 	const	tb = document.createElement('table')
 	
@@ -63,48 +61,11 @@
 	tb.append(content, layout, tags)
 	document.querySelector('#header').insertAdjacentElement("afterend", tb)
 
-	//	IDs
-	content.id = "td-1"
-	layout.id = "td-2"
-
-	//	Position elements in the DOM
-
-	// document.querySelector('#content').append(tb, rc)
-
-	
-	// const img = document.querySelector(`img#image`);
-	// console.log(img);
-
-	// const maxWidth = img.width * Math.min(1, FRAME_WIDTH / img.width);
-	// const maxHeight = img.height * Math.min(1, FRAME_HEIGHT / img.height);
-
-	// const style = (img.height / img.width < 1) ? {
-	// 	height: "auto",
-	// 	// width: "calc(100vh - 113px)"
-	// 	width: "min(calc(100vh - 127px), 1200px)"
-	// } : {
-	// 	width: "auto",
-	// 	height: "min(calc(100vh - 127px))"
-	// };
-
 	content.append(img)
 
-	// while (img.previousElementSibling) img.previousElementSibling.remove();
-	// while (img.previousElementSibling)
-	// 	img.parentElement.insertBefore(img, img.previousElementSibling);
-
 	Object.assign(content.style, (aspect < 0.5) ? {
-		// height:	`calc(calc(100vh - ${16 + tb.offsetTop}px) * ${1 / aspect})`,
 		width:	`calc(100vh - ${16 + tb.offsetTop}px)`
 	} : {
-		// width:	`calc(calc(100vh - ${16 + tb.offsetTop}px) * ${aspect})`,
 		height:	`calc(100vh - ${16 + tb.offsetTop}px)`
 	});
-	// Object.assign(img.style, {
-	// 	width:	`min(calc(calc(100vh - 127px) * ${aspect}), max(calc(100vw * 0.70710678118), 1200))`,
-	// 	height:	`min(calc(100vh - 127px)), max(calc(100vw * 0.70710678118), 1200) * ${1 / aspect}))`
-	// });
-
-	
-	// Object.assign(img.style, style)
 })();
