2a5e0d8640c1e58485d2cc384abbfb9ab8386d77
nanojin <nanochemic@gmail.com>
2023-02-03 11:49:33 -0600

    modifying using form method

--- one_click.js
+++ one_click.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name			Image One Clicker
 // @namespace		http://nanosoft.net/
-// @version	  		1.1.3
+// @version	  		1.1.4
 // @description		AI domination!
 // @author			Nano
 // @match			*://*/*
@@ -14,16 +14,41 @@
 (function() {
 	'use strict';
 
-	const image_click = new AbortController()
-	const clickReport = (event) => {
-		//console.log(event.type)
-		if(MouseEvent.altKey === false){
-			return;
-		}
+	var history_map = new Map()
 
-		var a = document.createElement('a')
-		Object.assign(a, {download: ''})
-		//document.querySelectorAll(":hover img"
+	const image_click = new AbortController()
+	const form = document.createElement('form')
+	const clickReport = (event) => {
+		// if(MouseEvent.altKey === false){
+		// 	return;
+		// }	//	Check for modifier key combo
+
+		// var a = document.createElement('a')
+		// Object.assign(a, {download: ''})
+		// //document.querySelectorAll(":hover img"
+		// //).forEach(
+		// //  (img, index) => {
+		// //  Object.assign(a, {href: img.src})
+		// //  console.log(a)
+		// //  a.click()
+		// //})
+		// Array.from(
+		// 	document.elementsFromPoint(
+		// 		event.clientX,
+		// 		event.clientY
+		// 	)
+		// ).filter(
+		// 	e => {return e.localName == 'img'}
+		// ).forEach(
+		// 	img => {
+		// 		Object.assign(a, {href: img.src})
+		// 		console.log(a)
+		// 		a.click()
+		// 	}
+		// )
+
+		//	Attempting download using the Form element
+				//document.querySelectorAll(":hover img"
 		//).forEach(
 		//  (img, index) => {
 		//  Object.assign(a, {href: img.src})
@@ -36,16 +61,26 @@
 				event.clientY
 			)
 		).filter(
-			e => {return e.localName == 'img'}
+			e => {
+				return e.localName == 'img'	}
 		).forEach(
 			img => {
-				Object.assign(a, {href: img.src})
-				console.log(a)
-				a.click()
+				if(history_map[img.src])
+					return;
+				history_map[img.src] = true;
+				Object.assign(form, {
+					action: img.src
+				})
+				console.log(form)
+				form.submit()
 			}
 		)
 	}
+	Object.assign(form, {
+		target: "_blank"
+	})
 
+	document.body.append(form)
 	document.addEventListener("click", function (event) { clickReport(event) }, true, { signal: image_click.signal })
 })();
 
@@ -59,11 +94,13 @@
 		https://www.google.com/search?q=js+filter+array+by+element+type
 		https://www.google.com/search?q=js+filter+nodelist+by+element+type
 		https://www.google.com/search?q=js+check+if+button+is+held
+		https://www.google.com/search?q=Form+submission+canceled+because+the+form+is+not+connected
 	}
 	Stack Overflow:	{
 		https://stackoverflow.com/questions/4727919/get-selected-element-type
 		https://stackoverflow.com/questions/32765157/filter-or-map-nodelists-in-es6
 		https://stackoverflow.com/q/11620698/8499999 ← js download a link
+		https://stackoverflow.com/a/42081856/8499999 ← document.body.appendChild(form)
 	}
 	Links:	{
 		https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent
@@ -72,6 +109,8 @@
 		https://developer.mozilla.org/en-US/docs/Web/Events/Event_handlers
 		https://developer.mozilla.org/en-US/docs/Web/API/Document/elementFromPoint
 		https://developer.mozilla.org/en-US/docs/Web/API/Document/elementsFromPoint
+		https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
+		https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
 		https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
 	}
 	Icons:	{
